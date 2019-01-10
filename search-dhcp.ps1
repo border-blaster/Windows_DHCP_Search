a4224b1913a01059df61d5b04d8856324977fffe
border-blaster <45774002+border-blaster@users.noreply.github.com>
2019-01-10 13:58:48 -0700

    Update search-dhcp.ps1

--- search-dhcp.ps1
+++ search-dhcp.ps1
@@ -1,1 +1,6 @@
-## info come
+$MAC=$args[0]
+$dhcpservername = "PUTYOURSERVERNAMEHERE"
+
+if ($MAC -eq $null) { $MAC = read-host "MAC address (xx-xx-xx-xx-xx-xx)" }
+
+foreach ($scope in Get-DhcpServerv4Scope -ComputerName $dhcpservername){Get-DhcpServerv4Lease -ComputerName $dhcpservername -AllLeases -ScopeId $scope.ScopeId | Where-Object {$_.clientid -match $MAC} | fl}
