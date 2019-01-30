c7aeffa11135dcead2c15d96cfe67d5ee391cad7
border-blaster <45774002+border-blaster@users.noreply.github.com>
2019-01-30 10:59:47 -0700

    Update search-dhcp.ps1
    Added cleaning up of the mac address format.

--- search-dhcp.ps1
+++ search-dhcp.ps1
@@ -1,6 +1,32 @@
-$MAC=$args[0]
-$dhcpservername = "PUTYOURSERVERNAMEHERE"
+### Windows DHCP Search
+### github.com/border-blaster
+### v2019-01-30
+$OrginalMAC=$args[0]
 
-if ($MAC -eq $null) { $MAC = read-host "MAC address (xx-xx-xx-xx-xx-xx)" }
+### Enter in your DHCP server name:
+$dhcpservername = "SERVERNAME"
 
-foreach ($scope in Get-DhcpServerv4Scope -ComputerName $dhcpservername){Get-DhcpServerv4Lease -ComputerName $dhcpservername -AllLeases -ScopeId $scope.ScopeId | Where-Object {$_.clientid -match $MAC} | fl}
+### If the MAC was in an arugutment, get the MAC.
+if ($OrginalMAC -eq $null) { $OrginalMAC = read-host "MAC address" }
+
+### Clean up the MAC, unless it is a mess.
+# Lifted from: https://gist.github.com/border-blaster/9cc0ed0fa5967fafc301bb78d18a467e
+$CleanMAC = $OrginalMAC -replace '[-.:]',''
+if ($CleanMAC.length -ne 12) { write-host "MAC address is made of 12 alphanumeric characters"
+exit}
+
+#Dashes X 2
+$LocationCount = 2
+$AddCount = 3
+$WhatToInsert = "-"
+$CountLimit = "14"
+$InputMAC = $CleanMAC
+
+Do {
+	$InputMAC = $InputMAC.Insert($LocationCount,$WhatToInsert)
+	$LocationCount = $LocationCount + $AddCount
+	} until ($LocationCount -gt $CountLimit)
+$InputMAC
+
+
+foreach ($scope in Get-DhcpServerv4Scope -ComputerName $dhcpservername){Get-DhcpServerv4Lease -ComputerName $dhcpservername -AllLeases -ScopeId $scope.ScopeId | Where-Object {$_.clientid -match $InputMAC} | fl}
